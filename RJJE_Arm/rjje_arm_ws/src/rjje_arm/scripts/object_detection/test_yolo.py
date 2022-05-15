6912e655b4a49dfd1d53f180f6867a75f5b723e6
Rico_Jia <rjia@accelrobotics.com>
2022-05-15 15:32:25 -0700

    os kill successful

---
+++ RJJE_Arm/rjje_arm_ws/src/rjje_arm/scripts/object_detection/test_yolo.py
@@ -0,0 +1,41 @@
+from yolo_detect import YoloDetector
+from multiprocessing import Queue
+from multiprocessing import  Process
+input_queue = Queue(maxsize = 10)
+output_queue = Queue(maxsize = 10)
+path = "data/images/"
+
+import signal
+import os
+all_files = os.listdir(path)
+import cv2
+for file_name in all_files: 
+    file_path = os.path.join(path, file_name)
+    print(file_path)
+    img = cv2.imread(file_path, cv2.IMREAD_COLOR)
+    input_queue.put(img)
+
+# Need to launch the yolo detector and run together in a separate process
+def process_func(input_queue, output_queue): 
+    yolo_detector = YoloDetector(input_queue = input_queue, output_queue = output_queue)
+    yolo_detector.run()
+    print("finished run")
+proc = Process(target=process_func, args=(input_queue, output_queue))
+proc.start()
+# TODO
+import time 
+time.sleep(5)
+print("time to kill")
+
+os.kill(proc.pid, signal.SIGUSR2)
+proc.join()
+
+# clear queue 
+while not output_queue.empty(): 
+    output_queue.get(block=True, timeout=0.1)
+while not input_queue.empty(): 
+    input_queue.get(block=True, timeout=0.1)
+
+# cv2.imshow("sdf", img0)
+# cv2.waitKey(0)
+# print("finish run")
