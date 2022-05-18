831dda9c8a981bdb60378c8afd98b9d75ba759b6
rjia <rjia@accelrobotics.com>
2022-05-18 11:33:26 -0500

    updated the repo

--- RJJE_Arm/rjje_arm_ws/src/rjje_arm/scripts/object_detection/aruco_pose_estimate.py
+++ RJJE_Arm/rjje_arm_ws/src/rjje_arm/scripts/object_detection/aruco_pose_estimate.py
@@ -5,6 +5,7 @@
     - intrinsics of the camera (pinhole model)
 - Outputs: 
     - {marker_id: pose (4x4 np.ndarray) ...}
+- Similar Project: https://github.com/hm7455/Exhibition-hall-robot_vision/blob/main/aruco%20_mark/detect_aruco_dynamic_realsense_pub.py
 """
 import numpy as np
 from typing import Dict
@@ -30,11 +31,11 @@ class ArucoExtrinsics():
         rejected: A list of potential markers that were found but ultimately rejected due to the inner code of the marker being unable to be parsed (visualizing the rejected markers is often useful for debugging purposes)
         """
         # load aruco dictionary
-        aruco_dict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_4X4_50)
+        aruco_dict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_4X4_100)
         # Define the ArUco detection parameters using 
         parameters =  cv2.aruco.DetectorParameters_create()
         corners, ids, rejected = cv2.aruco.detectMarkers(img, aruco_dict, parameters=parameters)
-        print(ids.shape)
+        print(corners)
         return {ids[i][0]: corner_list for i, corner_list in enumerate(corners)}
 
     def _estimate_pose(self, aruco_detections: Dict): 
@@ -42,7 +43,7 @@ class ArucoExtrinsics():
 
 #TODO
 if __name__ == "__main__":
-    img = cv2.imread("data/images/aruco.jpg", cv2.IMREAD_COLOR)
+    img = cv2.imread("data/images/aruco_2.jpg", cv2.IMREAD_COLOR)
     ae = ArucoExtrinsics()
     ae.calibrate(img)
     cv2.imshow("img", img)
