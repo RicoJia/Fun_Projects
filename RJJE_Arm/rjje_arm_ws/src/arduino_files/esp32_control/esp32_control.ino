f44092eec4d4dbda723cd169ac0d06ab89a44fcf
ricojia <ricoruotongjia@gmail.com>
2022-06-07 22:35:00 -0500

    mosq

--- RJJE_Arm/rjje_arm_ws/src/arduino_files/esp32_control/esp32_control.ino
+++ RJJE_Arm/rjje_arm_ws/src/arduino_files/esp32_control/esp32_control.ino
@@ -4,11 +4,115 @@
   Introduction:
     1. ESP32 Receive 5 servo angle values and 1 gripper command through wifi.
     2. Display mode of operation
-
-  Modes of operations:
-      1. Teaching mode: /rjje_arm/motion_control is [270, 270,270, 270,270, 270]. To turn it off, [360, 360,360, 360,360, 360]
-      2. Regular Mode: listens to /rjje_arm/motion_control, and execute joint angles within execution_time. The upper stream python node can replay recorded angles, too. Note, only angles within (0, 180) will be executed
-  Notes: 
-      1. MG996R has a different rotation orientation than FB5116M. So the default flip_rotation_ flag is different. Their pwm range is also slightly different.
+    3. ref: https://randomnerdtutorials.com/esp32-mqtt-publish-subscribe-arduino-ide/
+        - how to start mosquitto: https://blog.csdn.net/weixin_44901301/article/details/108031063
+        - You need to set mosquitto.conf to whitelist listeners http://www.steves-internet-guide.com/mossquitto-conf-file/
+        - can manually load mosquitto: mosquitto [-c config] https://blog.csdn.net/houjixin/article/details/84539199
+            - kill mosquitto before you do this
 */
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <Wire.h>
+const char* ssid     = "Unit-380";
+const char* password = "3ecbe779";
 
+// Add your MQTT Broker IP address, example:
+//const char* mqtt_server = "192.168.1.144";
+const char* mqtt_server = "100.65.212.18";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+long lastMsg = 0;
+char msg[50];
+int value = 0;
+
+void setup_wifi() {
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* message, unsigned int length) {
+  Serial.print("Message arrived on topic: ");
+  Serial.print(topic);
+  Serial.print(". Message: ");
+  String messageTemp;
+  
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)message[i]);
+    messageTemp += (char)message[i];
+  }
+  Serial.println();
+
+  // Feel free to add more if statements to control more GPIOs with MQTT
+
+  // If a message is received on the topic esp32/output, you check if the message is either "on" or "off". 
+  // Changes the output state according to the message
+  if (String(topic) == "esp32/output") {
+    Serial.print("Changing output to ");
+    if(messageTemp == "on"){
+      Serial.println("on");
+    }
+    else if(messageTemp == "off"){
+      Serial.println("off");
+    }
+  }
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("ESP32Client")) {
+      Serial.println("connected");
+      // Subscribe
+      client.subscribe("esp32/output");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  setup_wifi();
+  client.setServer(mqtt_server, 8883);
+  client.setCallback(callback);
+}
+
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  long now = millis();
+  if (now - lastMsg > 5000) {
+    float humidity = 34.5;
+    // Convert the value to a char array
+    char humString[8];
+    dtostrf(humidity, 1, 2, humString);
+    Serial.print("Humidity: ");
+    Serial.println(humString);
+    client.publish("esp32/humidity", humString);
+  }
+}
