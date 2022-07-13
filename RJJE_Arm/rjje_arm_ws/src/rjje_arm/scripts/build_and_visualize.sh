01bfcd8c0b4496f225862d31fb76109ad39576a3
ricojia <ricoruotongjia@gmail.com>
2022-07-12 21:21:46 -0500

    ros control works!

--- RJJE_Arm/rjje_arm_ws/src/rjje_arm/scripts/build_and_visualize.sh
+++ RJJE_Arm/rjje_arm_ws/src/rjje_arm/scripts/build_and_visualize.sh
@@ -1,4 +1,11 @@
+# This file can be used to launch rviz or gazebo models
 # assume we have sourced the workspace
 cd ../urdf
 xacro rjje_arm.xacro > rjje_arm.xacro.urdf
-roslaunch rjje_arm rjje_arm_visualization.launch
+# roslaunch rjje_arm rjje_arm_visualization.launch
+
+if [ ${1} == "gazebo" ]; then
+    roslaunch rjje_arm gazebo.launch
+else
+    roslaunch rjje_arm rjje_arm_visualization.launch
+fi
