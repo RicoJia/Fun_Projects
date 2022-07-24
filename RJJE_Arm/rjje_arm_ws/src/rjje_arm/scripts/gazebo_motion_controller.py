3bc885a79c1c759fa37a40017ea8b443188f8764
ricojia <ricoruotongjia@gmail.com>
2022-07-24 18:41:18 -0500

    motion controller: hand not moving

--- RJJE_Arm/rjje_arm_ws/src/rjje_arm/scripts/gazebo_motion_controller.py
+++ RJJE_Arm/rjje_arm_ws/src/rjje_arm/scripts/gazebo_motion_controller.py
@@ -18,7 +18,8 @@ from utils.rico_mqtt import MqttClient, MqttSubscriberCbs
 
 
 class Params:
-    JOINT_NUM = 5
+    ARM_JOINTS_NUM = 5
+    HAND_JOINTS_NUM = 1
     TIME_NUM=1
     EXECUTION_PUBLISH_FREQ = 100
     
@@ -29,21 +30,24 @@ class GazeboMotionController:
         sub_topics = [topic for topic, *_ in subs]
         # filter returns a filter object, cannot be unpacked directly
         joint_topics = list (filter(lambda topic: "command" in topic, sub_topics))
-        self.publishers = tuple(((topic, rospy.Publisher(topic, Float64, queue_size=10) ) for topic in joint_topics))
+        publishers = tuple(((topic, rospy.Publisher(topic, Float64, queue_size=10) ) for topic in joint_topics))
+        self.arm_publishers = publishers[:-1]
+        self.hand_publishers = [publishers[-1]]
             
         print("Recording joints: ", joint_topics)
         
         self.mqtt_client = MqttClient("127.0.0.1", 1883,
                         {
-                            "esp/plan": self.plan_cb,
-                            "esp/hand": self.hand_cb
+                            "esp/plan": self.arm_joints_cb,
+                            "esp/hand": self.hand_joint_cb
                         }, 
                         "gazebo_motion_controller")
     
         self.rate = rospy.Rate(Params.EXECUTION_PUBLISH_FREQ)
        
         self.gazebo_joint_state_sub = rospy.Subscriber("/rjje_arm_gazebo/joint_states", JointState, self.gazebo_joint_state_cb) 
-        self.joint_states = []
+        self.arm_joint_states = []
+        self.hand_joint_states = []
 
     def gazebo_joint_state_cb(self, msg):
         '''
@@ -53,37 +57,53 @@ class GazeboMotionController:
         names = ";".join(msg.name)
         positions = ";".join([str(p) for p in msg.position])
         self.mqtt_client.publish("esp/joint_states", names + "|" + positions)
-        self.joint_states = msg.position
+        self.arm_joint_states = msg.position[:-1]
+        self.hand_joint_states = [msg.position[-1]] 
+    
+    def __execute_plans(self, userdata, msg, publishers: list, joint_states: list):
+        """
+        Parse plans that look like: val_a1; val_a2; ... time_1;| val_b1, ... time_2;| ... 
+        Then execute each plan according to its time
+
+        Args:
+            userdata (_type_): Custom User data 
+            msg (_type_): Actual MQTT Message
+            publishers (list): Length of each plan
+
+        Returns:
+        """
+        plans = []
+        plan_length = len(publishers) + 1
+        commands = str(msg.payload.decode("utf-8")).split("|")
+        for command in commands:
+            commanded_angles_and_time = MqttSubscriberCbs.get_array_from_string(command.split(";"))
+            if len(commanded_angles_and_time) == plan_length: 
+                plans.append(commanded_angles_and_time)
+
+        for plan in plans:
+            execution_time = plan[-1]
+            num_intervals = int(np.ceil(execution_time * Params.EXECUTION_PUBLISH_FREQ))
+            delta_intervals = (np.array(plan[:-1]) - np.array(joint_states))/num_intervals
+            for interval in range(num_intervals):
+                for i in range(plan_length - 1):
+                    topic, pub = publishers[i]
+                    angle_to_pub = plan[i] - (num_intervals - interval + 1) * delta_intervals[i]
+                    pub.publish(Float64(angle_to_pub))
+                    #TODO
+                    print(angle_to_pub)
+                self.rate.sleep()
             
-    def plan_cb(self, userdata, msg):
+    def arm_joints_cb(self, userdata, msg):
         """
         Each plan has 5 joint angles 3 digit precision + execution time. Ex: 12.3;32.2;23.0;45.4;66.2;0.02;|... , where 0.02 is the execution time*/
         """
-        plans = []
-        commands = str(msg.payload.decode("utf-8")).split("|")
-        for command in commands:
-            commanded_angles = MqttSubscriberCbs.get_array_from_string(command.split(";"))
-            if len(commanded_angles) == Params.JOINT_NUM + Params.TIME_NUM:
-                plans.append(commanded_angles)
-        #TODO - to finish smooth execution
-        print(plans)
-        for plan in plans:
-            execution_time = plan[Params.JOINT_NUM]
-            num_intervals = int(np.ceil(execution_time * Params.EXECUTION_PUBLISH_FREQ))
-            delta_intervals = (np.array(plan[:Params.JOINT_NUM]) - np.array(self.joint_states[:Params.JOINT_NUM]))/num_intervals
-            for interval in range(num_intervals):
-                for i in range(Params.JOINT_NUM):
-                    topic, pub = self.publishers[i]
-                    angle_to_pub = plan[i] - (num_intervals - interval + 1) * delta_intervals[i]
-                    # print(plan[i])
-                    pub.publish(Float64(angle_to_pub))
-                self.rate.sleep()
+        self.__execute_plans(userdata, msg, self.arm_publishers, self.arm_joint_states)
 
-    def hand_cb(self, userdata, msg):
+    def hand_joint_cb(self, userdata, msg):
         """
         Payload = 0, claw will be closed, 1 will be open
         """
-        pass
+        self.__execute_plans(userdata, msg, self.hand_publishers, self.hand_joint_states)
     
 if __name__ == '__main__':
     rospy.init_node("gazebo_motion_controller")
